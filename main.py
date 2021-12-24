4e8e39596087f7d6381e78417a753dcf8f8df9e8
MiFed <71005413+Mifed1@users.noreply.github.com>
2021-12-24 14:45:42 +0400

    Small update

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def help(ctx):
 
 #   Category Help Commands
 
-@help.command(aliases=['command', 'commands', 'cmds'])
+@help.command(aliases=['command', 'commands'])
 async def cmd(ctx):
     em = discord.Embed(
         title="Commands help menu",
@@ -61,6 +61,23 @@ async def cmd(ctx):
     em.add_field(name=":moneybag: Economy", value="`balance`, `pay`, `deposit`, `withdraw`, `beg`, `rob`", inline=False)
     em.add_field(name=":wrench: Utility", value="`test`, `test`", inline=False)
     em.add_field(name=":8ball: Fun", value="`test`, `test`", inline=False)
+    em.timestamp = datetime.datetime.utcnow()
+    em.set_author(name=client.user.name, url="", icon_url=client.user.avatar.url)
+    em.set_footer(text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
+
+
+    await ctx.reply(embed = em)
+
+@help.command(aliases=['cl'])
+async def commandlist(ctx):
+    em = discord.Embed(
+        title="Commands help menu",
+        description=f"All commands\nFor specific command help do `{PREFIX}help <command>`"
+    )
+    em.add_field(name=":information_source: Info", value="1 Command", inline=False)
+    em.add_field(name=":moneybag: Economy", value="1 Command", inline=True)
+    em.add_field(name=":wrench: Utility", value="1 Command", inline=False)
+    em.add_field(name=":8ball: Fun", value="1 Command", inline=True)
     em.timestamp = datetime.datetime.utcnow()
     em.set_author(name=client.user.name, url="", icon_url=client.user.avatar.url)
     em.set_footer(text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
